10296b95820874b2d9dd53a8f52d2f9ae4ca31d0
ogami334 <ogami-takuya40439@g.ecc.u-tokyo.ac.jp>
2022-07-20 03:56:08 +0900

    feat: add input functions and some import

---
+++ src/sub_template.py
@@ -0,0 +1,34 @@
+from typing import Any, Callable, List, Tuple
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+
+# constant
+inf = 10 ** 20
+mod = 10 ** 9 + 7
+# mod = 998244353
+
+
+# input functions
+def OneI():
+    return int(input())
+
+
+def MulI():
+    return map(int, input().split())
+
+
+def ListI():
+    return list(map(int, input().split()))
+
+
+def StrI():
+    return input().rstrip()
+
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    main()
